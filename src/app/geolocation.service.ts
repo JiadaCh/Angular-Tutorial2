80c9b277671cc1717db598d109d678a8ce6d6518
Jiada <124351114+JiadaCh@users.noreply.github.com>
2024-02-18 18:59:06 +0100

    a

--- src/app/geolocation.service.ts
+++ src/app/geolocation.service.ts
@@ -12,14 +12,21 @@ export class GeolocationService {
     if (house){
       return [house.latitude,house.longitude]
     }else{
+      console.log("No se ha encontrado la localización")
       return [0,0]
     }
 
   }
+
   public getUserLocation(){
+
     navigator.geolocation.getCurrentPosition(
       ({coords}) =>{
         this.userLocalitation=  [coords.latitude,coords.longitude]
+      },
+      ()=>{
+        this.userLocalitation =[0,0];
+        console.log("No se ha encontrado la geolocalización")
       }
     )
   }
